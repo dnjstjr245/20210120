db88c4171498a5dbe6af205dbb925cc158a68df0
wonseok <dnjstjr245@gmail.com>
2021-01-20 13:39:45 +0900

    version 20210120

---
+++ members/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from .models import Members
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>hello<h1>")
+
+def test(request):
+    return HttpResponse("<h2>Test<h2>")
+
+def signup(req):
+    if req.method == 'POST' :
+        username = req.POST['username']
+        email = req.POST['email']
+
+        member = Members(
+            username = username,
+            useremail = email
+        )
+
+        member.save()
+
+        res_data ={}
+        res_data['res'] = '등록성공'
+
+        return render(req, 'index.html', res_data)
+    return render(req, 'index.html')
